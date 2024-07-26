d8628b310252fd3536f998384de410ebab838580
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-07-26 15:59:52 +0530

    Adding dotenv file

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -4,13 +4,23 @@ import { getAuth } from 'firebase/auth';
 // import dotenv from "dotenv"
 // dotenv.config()
 
+// const firebaseConfig = {
+//   apiKey: String(import.meta.env.VITE_FIREBASE_API),
+//   authDomain: String(import.meta.env.VITE_FIREBASE_DOMAIN),
+//   projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
+//   storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
+//   messagingSenderId:String(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+//   appId: String(import.meta.env.VITE_FIREBASE_APP_ID)
+// };
+
+
 const firebaseConfig = {
-  apiKey: String(import.meta.env.VITE_FIREBASE_API_KEY),
-  authDomain: String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
-  projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
-  storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
-  messagingSenderId:String(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
-  appId: String(import.meta.env.VITE_FIREBASE_APP_ID)
+  apiKey: "AIzaSyCyOAWbliXKcGpCiWEs1uDcRydgFQn7c3Y",
+  authDomain: "estore-27700.firebaseapp.com",
+  projectId: "estore-27700",
+  storageBucket: "estore-27700.appspot.com",
+  messagingSenderId: "454923885240",
+  appId: "1:454923885240:web:a740296213050c6ac2b24d"
 };
 
 const app = initializeApp(firebaseConfig);
